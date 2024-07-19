16a6c98c8e43e7fc9cecfbf17142abd7713a36da
Arpan Goswami <arpan.goswami21@gmail.com>
2024-07-19 13:16:10 +0530

    validation and syntax enhancement

--- app/models/testimonial.rb
+++ app/models/testimonial.rb
@@ -1,6 +1,6 @@
 class Testimonial < ApplicationRecord
   has_one_attached :avatar
   validates :name, :quote, presence: true
-  validates :email, uniqueness: true
-  validates_format_of :name, :with => /\A[a-z\s]+\z/i
+  validates :email, uniqueness: true, allow_blank: true
+  validates_format_of :name, with: /\A[a-z\s]+\z/i
 end
--- app/models/visitor.rb
+++ app/models/visitor.rb
@@ -2,5 +2,5 @@ class Visitor < ApplicationRecord
   validates :email, :first_name, :last_name, presence: true
   validates :email, uniqueness: true
   validates :phone_number, length: {minimum:5, maximum:12}
-  validates_format_of :first_name, :last_name, :with => /\A[a-z\s]+\z/i
+  validates_format_of :first_name, :last_name, with: /\A[a-z\s]+\z/i
 end
