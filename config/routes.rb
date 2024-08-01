b109a6fd43987c2ff761225d371c7231a8c9a254
Arpan Goswami <arpan.goswami21@gmail.com>
2024-08-01 13:10:53 +0530

    response fix

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   post 'visitors', to: 'visitors#create'
   get 'download_cv', to: 'documents#download_cv'
   resources :testimonials, only: [:create, :index]
-  get 'health_check', to: proc { [200, {"message": "Whatup World!"}, ['success']] }
+  get 'health_check', to: proc { [200, {"message" => "Whatup World!"}, ['success']] }
 
 end
