aec99b94df684e52a9647c7b0332fdc321efe0ef
Arpan Goswami <arpan.goswami21@gmail.com>
2024-08-04 14:47:44 +0530

    removing multiline code

--- lib/mailgun_service.rb
+++ lib/mailgun_service.rb
@@ -5,12 +5,7 @@ class MailgunService
   def self.send_email(mail, reciever)
     api_key = ENV['MAILGUN_API_KEY']
     domain = ENV['MAILGUN_DOMAIN']
-    data = {
-      from: 'arpan.goswami21@gmail.com',
-      to: reciever.email,
-      subject = mail.subject,
-      html: mail.body.encoded
-    }
+    data = { from: 'arpan.goswami21@gmail.com', to: reciever.email, subject = mail.subject, html: mail.body.encoded }
     response = RestClient.post("https://api.mailgun.net/v3/#{domain}/messages", data, { Authorization: "Basic " + Base64.strict_encode64("api:#{api_key}") })
   end
 
